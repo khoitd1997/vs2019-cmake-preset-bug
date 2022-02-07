da9aa29c13164e21ac51e0333d84261ed81f45ce
Khoi Trinh <khoidinhtrinh@gmail.com>
2022-02-06 22:37:53 -0800

    added base project

---
+++ a53_toolchain.cmake
@@ -0,0 +1,12 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(CMAKE_SYSROOT "C:/Xilinx/Vitis/2020.1/gnu/aarch64/nt/aarch64-linux/aarch64-xilinx-linux")
+
+set(CMAKE_C_COMPILER "C:/Xilinx/Vitis/2020.1/gnu/aarch64/nt/aarch64-linux/bin/aarch64-linux-gnu-gcc.exe")
+set(CMAKE_CXX_COMPILER "C:/Xilinx/Vitis/2020.1/gnu/aarch64/nt/aarch64-linux/bin/aarch64-linux-gnu-g++.exe")
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
