d929867b3e8391b91a22a066559a417a0b59e687
Khoi Trinh <khoidinhtrinh@gmail.com>
2022-02-26 14:12:02 -0800

    added gcc arm toolchain

--- a53_toolchain.cmake
+++ a53_toolchain.cmake
@@ -3,11 +3,11 @@ set(CMAKE_SYSTEM_PROCESSOR arm)
 
 set(CMAKE_TRY_COMPILE_TARGET_TYPE "STATIC_LIBRARY")
 
-set(CMAKE_SYSROOT "C:/Users/khoid/Downloads/gcc-arm-10.3-2021.07-mingw-w64-i686-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc")
+set(CMAKE_SYSROOT "${CMAKE_CURRENT_SOURCE_DIR}/gcc-arm-10.3-2021.07-mingw-w64-i686-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc")
 # set(CMAKE_FIND_ROOT_PATH "C:/Users/khoid/Downloads/gcc-arm-10.3-2021.07-mingw-w64-i686-aarch64-none-linux-gnu")
 
-set(CMAKE_C_COMPILER "C:/Users/khoid/Downloads/gcc-arm-10.3-2021.07-mingw-w64-i686-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-gcc.exe")
-set(CMAKE_CXX_COMPILER "C:/Users/khoid/Downloads/gcc-arm-10.3-2021.07-mingw-w64-i686-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-g++.exe")
+set(CMAKE_C_COMPILER "${CMAKE_CURRENT_SOURCE_DIR}/gcc-arm-10.3-2021.07-mingw-w64-i686-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-gcc.exe")
+set(CMAKE_CXX_COMPILER "${CMAKE_CURRENT_SOURCE_DIR}/gcc-arm-10.3-2021.07-mingw-w64-i686-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-g++.exe")
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
