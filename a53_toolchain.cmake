a753fbc6720aedc75191d50ec801d951cfe6fcea
Khoi Trinh <khoidinhtrinh@gmail.com>
2022-03-25 21:24:47 -0700

    added sysroot stuffs

--- a53_toolchain.cmake
+++ a53_toolchain.cmake
@@ -3,11 +3,9 @@ set(CMAKE_SYSTEM_PROCESSOR arm)
 
 set(CMAKE_TRY_COMPILE_TARGET_TYPE "STATIC_LIBRARY")
 
-set(CMAKE_SYSROOT "${CMAKE_CURRENT_SOURCE_DIR}/gcc-arm-10.3-2021.07-mingw-w64-i686-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc")
-# set(CMAKE_FIND_ROOT_PATH "C:/Users/khoid/Downloads/gcc-arm-10.3-2021.07-mingw-w64-i686-aarch64-none-linux-gnu")
-
-set(CMAKE_C_COMPILER "${CMAKE_CURRENT_SOURCE_DIR}/gcc-arm-10.3-2021.07-mingw-w64-i686-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-gcc.exe")
-set(CMAKE_CXX_COMPILER "${CMAKE_CURRENT_SOURCE_DIR}/gcc-arm-10.3-2021.07-mingw-w64-i686-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-g++.exe")
+set(CMAKE_SYSROOT "${CMAKE_CURRENT_SOURCE_DIR}/gcc-arm-10.3-2021.07-mingw-w64-i686-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc" CACHE PATH "Sysroot" FORCE)
+set(CMAKE_C_COMPILER "${CMAKE_CURRENT_SOURCE_DIR}/gcc-arm-10.3-2021.07-mingw-w64-i686-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-gcc.exe" CACHE FILEPATH "gcc" FORCE)
+set(CMAKE_CXX_COMPILER "${CMAKE_CURRENT_SOURCE_DIR}/gcc-arm-10.3-2021.07-mingw-w64-i686-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-g++.exe" CACHE FILEPATH "g++" FORCE)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+// #include <cstdint>
+#include <stdint.h>
 
 int main() {
+	uint8_t testNum;
+
   std::cout << "Hello there" << std::endl;
   return 0;
 }
